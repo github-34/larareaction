6eef83061d5e4afb8f56a4243041b93286719e10
Geo <13597665+github-34@users.noreply.github.com>
2022-11-24 01:33:53 -0500

    Fixed: cleanup of routes files

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,7 @@
 <?php
 
-use App\Models\Image;
-use App\Models\User;
-
-use App\Express\Models\Expression;
-use App\Express\ExpressionService;
-use App\Express\Xpress;
-use App\Express\Facades\Express;
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Insomnicles\Laraexpress\ExpressionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,33 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// auth:sanctum
-// Route::middleware('')->get('user', function (Request $request) {
-//     return $request->user();
-// });
-
-// Route::get('imageexpress', function (Request $request) {
-//     Auth::login(User::find(1));
-//     $image = Image::all()->first();
-//     Express::express($image, Xpress::FIVESTAR, Xpress::FIVESTARS);
-//     Express::express($image, Xpress::MICHELINSTAR, Xpress::TWOMICHELINSTARS);
-//     Express::express($image, Xpress::EMOTIVE, Xpress::HAPPY);
-
-//     return Expression::where('expressable_id', $image->id)->where('expressable_type','App\Models\Image')->where('user_id',1)->get();
-// });
-
-// Route::get('imageexpressioninfo', function (Request $request, ExpressionService $service) {
-//     Auth::login(User::find(1));
-//     $images = Image::all();
-//     return $service->obtainExpressableInfo($images);
-// });
-
-
 Route::middleware(['auth'])->group(function () {
-    Route::get('expressions',                 [App\Express\ExpressionController::class, 'index'])->name('api.expressions.index');
-    Route::get('expressions/{expression}',    [App\Express\ExpressionController::class, 'show'])->name('api.expressions.show');
-    Route::post('expressions',                [App\Express\ExpressionController::class, 'storeOrUpdate'])->name('api.expressions.store');
-    Route::patch('expressions/{expression}',  [App\Express\ExpressionController::class, 'update'])->name('api.expressions.update');
-    Route::delete('expressions/{expression}',               [App\Express\ExpressionController::class, 'destroy'])->name('api.expressions.destroy');
+    Route::get('expressions',                 [ExpressionController::class, 'index'])->name('api.expressions.index');
+    Route::get('expressions/{expression}',    [ExpressionController::class, 'show'])->name('api.expressions.show');
+    Route::post('expressions',                [ExpressionController::class, 'storeOrUpdate'])->name('api.expressions.store');
+    Route::patch('expressions/{expression}',  [ExpressionController::class, 'update'])->name('api.expressions.update');
+    Route::delete('expressions/{expression}', [ExpressionController::class, 'destroy'])->name('api.expressions.destroy');
 });
-Route::post('expressions/stats',              [App\Express\ExpressionController::class, 'stats'])->name('api.expressions.stats');
+Route::post('expressions/stats',              [ExpressionController::class, 'stats'])->name('api.expressions.stats');
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
 
-use App\Models\Image;
-
-use App\Express\ExpressionService;
-
 use Illuminate\Support\Facades\Route;
+use Insomnicles\Laraexpress\ExpressionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,22 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/dashboard', function (ExpressionService $service) {
-//     return view('dashboard', [
-//             'images' => $service->obtainExpressableInfo(Image::all())
-//         ]);
-// })->middleware(['auth'])->name('dashboard');
-
-
 Route::middleware(['auth'])->group(function () {
-    Route::get('expressions/{expression}',         [App\Express\ExpressionController::class, 'show'])->name('web.expressions.show');
-    Route::post('expressions',                    [App\Express\ExpressionController::class, 'storeOrUpdate'])->name('web.expressions.store');
-    Route::patch('expressions/{expression}',         [App\Express\ExpressionController::class, 'update'])->name('web.expressions.update');
-    Route::delete('expressions/{expression}',                 [App\Express\ExpressionController::class, 'destroy'])->name('web.expressions.destroy');
+    Route::get('expressions/{expression}',        [ExpressionController::class, 'show'])->name('web.expressions.show');
+    Route::post('expressions',                    [ExpressionController::class, 'storeOrUpdate'])->name('web.expressions.store');
+    Route::patch('expressions/{expression}',      [ExpressionController::class, 'update'])->name('web.expressions.update');
+    Route::delete('expressions/{expression}',     [ExpressionController::class, 'destroy'])->name('web.expressions.destroy');
 });
-
-//require __DIR__.'/auth.php';
